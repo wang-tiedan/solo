fa84c6549bedb43e0b3f776c6f24390d24775ad2
Wang Xiao Man <gemwxm@163.com>
2024-05-04 19:59:28 +0100

    Add import info, provisional annotate function process_payment

--- shopping/views.py
+++ shopping/views.py
@@ -5,12 +5,11 @@ import plotly.express as px
 from plotly.offline import plot
 from django.core.paginator import Paginator
 from django.contrib.auth import authenticate, login, logout
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, JsonResponse
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from shopping.models import Cart, CartItem
-from django.http import JsonResponse
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.views.decorators.csrf import csrf_protect
